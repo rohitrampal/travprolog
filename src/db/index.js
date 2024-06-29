57cbf79b7cdc535f396f93a815914ac2931b17bd
rohitrampal <thakurrohit210302@gmail.com>
2024-06-29 21:06:57 +0530

    alternate way to connect with db

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ export  const connectDb = async ()=>{
     try {
         const connect = await mongoose.connect(`mongodb://127.0.0.1:27017/travprologger`)
         console.log("MongoDb connected successfully");
-        console.log(connect.connection);
+        // console.log(connect.connection.);
         
     } catch (error) {
         console.log("----------------------Error in Connecting Database -----------\n",error);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,64 @@
-// import express form "express";
-// const express = require('express')
-import express from "express";
+import { error, log } from "console";
 import { connectDb } from "./db/index.js"
 import dotenv from "dotenv";
+import {app} from "./app.js";
 
 
 dotenv.config({
     path:"./env"
 })
 
-const app = express()
+const PORT = process.env.PORT || 1111;
 
-app.listen(process.env.PORT,()=>{
-    `server listen at port ${process.env.PORT}`
-});
+
 
 connectDb()
+.then(()=>{
+    app.on("error",()=>{
+        console.log("error: ---!---",error);
+        throw error
+    })
+    app.listen(PORT,()=>{
+        console.log(`server listen at port ${PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log("Database connection failed !!!!!!",error);
+})
 
+
+
+
+// -----------alternate way all the code is spred in to the ---------db/index.js---------src/constants.js===app.js====index.js-----------
+// by the use of iffe function 
+/*
+import express from "express";
+
+const app = express()
+PORT = process.env.PORT || 1111;
+
+( async ()=>{
+    try {
+        const connect = await mongoose.connect(`mongodb://127.0.0.1:27017/travprologger`)
+        
+        console.log("MongoDb connected successfully");
+        console.log(connect.connection);
+        
+        app.on("error",()=>{
+        console.log("error: ---!---",error);
+        throw error
+        })
+        
+        app.listen(PORT,()=>{
+            console.log(`server listen at port ${PORT}`);
+        });
+
+        
+    } catch (error) {
+        console.log("----------------------Error in Connecting Database -----------\n",error);
+        // throw error;
+        process.exit(1)
+        
+    }
+
+*/
